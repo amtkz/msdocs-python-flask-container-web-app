8155108014010aa6369f47fa23458b637b42879b
Amitkumar Dubey <amitkumar.dubey@guycarp.com>
2025-07-10 19:04:29 +0530

    added and remove files

--- iac/acr.tf
+++
@@ -1,8 +0,0 @@
-# This should only be created once, and in the production environment
-resource "azurerm_container_registry" "registry" {
-  name                = "${var.project}registry" # Must be alphanumeric
-  resource_group_name = azurerm_resource_group.hub.name
-  location            = azurerm_resource_group.hub.location
-  sku                 = "Standard"
-  admin_enabled       = true # required for App Service
-}
